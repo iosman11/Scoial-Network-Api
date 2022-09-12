4583fd2d3b27c667ea50ea1092ac27826ded6e6f
idil osman <idil_abukar@live.se>
2022-09-12 04:47:52 +0100

    readme

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // connecting server to mongoose
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/', {
+mongoose.connect('mongodb://127.0.0.1:27017/social-network', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });

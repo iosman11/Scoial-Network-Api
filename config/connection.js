0d53fbc98ebb3e51d585f24243ba19123edec325
idil osman <idil_abukar@live.se>
2022-09-12 03:06:13 +0100

    made config -unable to run local server

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+// connecting server to mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = mongoose.connection;
